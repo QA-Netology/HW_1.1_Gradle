1ce1dfb469f00b35a38b43ad6d8fc3af649c6146
yashinaQA42 <presnyakovae@gmail.com>
2022-12-30 20:57:21 +0300

    Initial commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,23 @@
+package ru.netology.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void shouldReturnNull() {
+        int actual = service.remain(1000);
+        int expected = 0;
+        Assert.assertEquals(expected,actual);
+        //TODO: You need to fix that
+    }
+
+    @Test
+    public void shouldReturn200() {
+        int actual = service.remain(800);
+        int expected = 200;
+        Assert.assertEquals(expected,actual);
+    }
+}
